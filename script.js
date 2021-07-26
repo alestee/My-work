19fbba135924f605f78ffae2ab722074b0dd21a7
Alexis Steckel <steckelalexis98@gmail.com>
2021-07-26 16:29:21 -0700

    my first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+script.js
+const form = document.getElementById('form')
+const input = document.getElementById('input')
+const todosUL = document.getElementById('todos') // referencing HTML ID's 
+
+function addTodo(todo) { 
+    let todoText = input.nodeValue;
+}
