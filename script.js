93a0464a5fdec3dc578e20230edf88a2e691b5a5
Alexis Steckel <steckelalexis98@gmail.com>
2021-08-04 17:55:15 -0700

    update

--- script.js
+++
@@ -1,8 +0,0 @@
-script.js
-const form = document.getElementById('form')
-const input = document.getElementById('input')
-const todosUL = document.getElementById('todos') // referencing HTML ID's 
-
-function addTodo(todo) { 
-    let todoText = input.nodeValue;
-}
